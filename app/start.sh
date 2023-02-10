3740ddb7dc194ed7c1f39f0073024a4ae599fbaa
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2023-02-10 17:03:17 +0200

    Create start.sh

---
+++ app/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
