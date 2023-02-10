a76e483ef107e9c16911b76a3fdd316a900a3c10
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2023-02-10 17:23:55 +0200

    Delete start.sh

--- app/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
