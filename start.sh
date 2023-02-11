61902dceda0893d33fef0e6ef7b8ddadc3834572
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2023-02-11 17:52:13 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
-do
-    sleep 0.1
-done
-sleep infinity
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-app --advertise-exit-node
