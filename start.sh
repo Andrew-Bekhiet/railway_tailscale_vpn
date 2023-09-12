4aaea2ccc1329ad0163d5a620e4ed6818939fd41
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2023-09-13 02:34:44 +0300

    fix: add advertise exit node

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-until ./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} ${TAILSCALE_ADDITIONAL_ARGS}
+until ./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS}
 do
     sleep 0.1
 done
