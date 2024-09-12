70ab79f3271d99fc28dd60686c9c21d8120691c4
HomeDev68 <samhomelabs68@proton.me>
2024-09-12 22:44:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 ./tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
-until ./tailscale up ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS}
+until ./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS}
 do
     sleep 0.1
 done
+./tailscale up ssh
 sleep infinity
